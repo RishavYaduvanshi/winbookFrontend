8609ef0151a04d5d45c7013923dce00ba922aac8
Md Hasibur Rahman <77970653+HasibNirban@users.noreply.github.com>
2022-10-17 16:53:47 +0530

    fixed (#10)

--- src/componrnts/Photos.jsx
+++ src/componrnts/Photos.jsx
@@ -5,16 +5,16 @@ const Photos = () => {
   return (
     <ImageList cols={3} rowHeight={100}>
         <ImageListItem>
-            <img src='https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c?w=164&h=164&fit=crop&auto=format'
+            <img src='https://winbook.d3m0n1k.engineer/post/71/'
             alt=""
             />
         </ImageListItem>
         <ImageListItem>
-            <img src='https://images.unsplash.com/photo-1516802273409-68526ee1bdd6?w=164&h=164&fit=crop&auto=format'
+            <img src='https://winbook.d3m0n1k.engineer/post/48/'
             alt=''/>
         </ImageListItem>
         <ImageListItem>
-            <img src='https://images.unsplash.com/photo-1589118949245-7d38baf380d6?w=164&h=164&fit=crop&auto=format'
+            <img src='https://winbook.d3m0n1k.engineer/post/35/'
             alt=''/>
         </ImageListItem>
     </ImageList>
