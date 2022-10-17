5e3194a60566ea5d17101c47d9fe3e7e045e95b4
HasibNirban <hasibnirban007@gmail.com>
2022-10-17 16:59:04 +0530

    fix

--- src/componrnts/Photos.jsx
+++ src/componrnts/Photos.jsx
@@ -5,16 +5,16 @@ const Photos = () => {
   return (
     <ImageList cols={3} rowHeight={100}>
         <ImageListItem>
-            <img src='https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c?w=164&h=164&fit=crop&auto=format'
+            <img src='https://winbookbackend.d3m0n1k.engineer/media/posts/IMG_20221016_193241_055.jpg'
             alt=""
             />
         </ImageListItem>
         <ImageListItem>
-            <img src='https://images.unsplash.com/photo-1516802273409-68526ee1bdd6?w=164&h=164&fit=crop&auto=format'
+            <img src='https://winbookbackend.d3m0n1k.engineer/media/posts/IMG20220926111010_EuCORDS.jpg'
             alt=''/>
         </ImageListItem>
         <ImageListItem>
-            <img src='https://images.unsplash.com/photo-1589118949245-7d38baf380d6?w=164&h=164&fit=crop&auto=format'
+            <img src='https://winbookbackend.d3m0n1k.engineer/media/posts/IMG_20220320_123757.jpg'
             alt=''/>
         </ImageListItem>
     </ImageList>
