7e2623a925f5a60767ad0a4eb588a930ee0043c5
Md Hasibur Rahman <77970653+HasibNirban@users.noreply.github.com>
2023-02-15 17:43:42 +0530

    Update utils.js

--- src/componrnts/utils.js
+++ src/componrnts/utils.js
@@ -2,7 +2,7 @@
 
 export function getShowableText(text) {
 
-    text = (text + " ").replace(/(#.+?)\s/g, '<span style="color:#1976d2;">$1 </span>');
+    text = (text + " ").replace(/(#.+?)\b/g, '<span style="color:#1976d2;">$1 </span>');
     console.log(text);
     return text
 }
