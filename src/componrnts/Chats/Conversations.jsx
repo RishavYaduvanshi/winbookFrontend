66770cf9e4b43424cabf165c98e253c3621aad96
Gagandeep Singh Bhambrah <19cse223.gagandeepsinghbhambrah@giet.edu>
2023-03-14 17:32:31 +0000

    added examples of chat connections with websock

--- src/componrnts/Chats/Conversations.jsx
+++ src/componrnts/Chats/Conversations.jsx
@@ -3,6 +3,18 @@ import Messages from './Messages';
 import Box from '@mui/material/Box';
 
 const Conversations = () => {
+  const sock = new WebSocket(
+    "wss://winbookbackend.d3m0n1k.engineer/ws/chat/?" +
+      localStorage.getItem("authtoken")
+  );
+  sock.onmessage = function (e) {
+    console.log(e.data);
+    //use this method to update the messages in a chat window
+  };
+  sock.onopen = sock.onmessage;
+  sock.onerror = sock.onmessage;
+  sock.onclose = sock.onmessage;
+  console.log("I WAS EXEC", sock);
   return (
     <Box>
       <Messages />
--- src/componrnts/utils.js
+++ src/componrnts/utils.js
@@ -1,40 +1,74 @@
-
-
 export function getShowableText(text) {
-
-    text = (text + " ").replace(/(#.+?)\b/g, '<span style="color:#1976d2;">$1 </span>');
-    // console.log(text);
-    return text
+  text = (text + " ").replace(
+    /(#.+?)\b/g,
+    '<span style="color:#1976d2;">$1 </span>'
+  );
+  // console.log(text);
+  return text;
 }
 
 export function createRSAKeyPair() {
-    //TODO: creates a pair of RSA keys
+  //TODO: creates a pair of RSA keys
 }
 
 export function encryptRSA() {
-    //TODO : encrypts a message using RSA
+  //TODO : encrypts a message using RSA
 }
 
 export function decryptRSA() {
-    //TODO : decrypts a message using RSA
+  //TODO : decrypts a message using RSA
 }
 
 export function encryptAES() {
-    //TODO : encrypts a message using AES
+  //TODO : encrypts a message using AES
 }
 
 export function decryptAES() {
-    //TODO : decrypts a message using AES
+  //TODO : decrypts a message using AES
 }
 
 export function getHash() {
-    //TODO : generates a hash of a message
+  //TODO : generates a hash of a message
 }
 
 export function getSignature() {
-    //TODO : generates a signature of a message
+  //TODO : generates a signature of a message
 }
 
 export function verifySignature() {
-    //TODO : verifies a signature of a message
+  //TODO : verifies a signature of a message
+}
+
+export function getChatList() {
+  fetch("https://winbookbackend.d3m0n1k.engineer/message/get_chats/", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Token " + localStorage.getItem("authtoken"),
+    },
+  }).then((response) => {
+    if (response.status >= 200 && response.status < 300) {
+      response.json().then((data) => {
+        console.log(data);
+        //TODO : use data to populate the chat list
+      });
+    }
+  });
+}
+
+export function getChatMessages(pk, page) {
+  fetch("https://winbookbackend.d3m0n1k.engineer/message/" + pk + "/", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Token " + localStorage.getItem("authtoken"),
+    },
+  }).then((response) => {
+    if (response.status >= 200 && response.status < 300) {
+      response.json().then((data) => {
+        console.log(data);
+        //TODO : use data to populate the chat messages
+      });
+    }
+  });
 }
